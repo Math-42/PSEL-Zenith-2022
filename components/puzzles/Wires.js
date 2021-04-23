9c45422df58ba0e085216e9127c08ca8ccc7d525
Math-42 <matheus.v4200@gmail.com>
2021-04-22 21:59:28 -0300

    criando desafio dos fios

--- components/puzzles/Wires.js
+++ components/puzzles/Wires.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import TextInfo from '../TextInfo';
+import TextInput from '../TextInput';
 
 export default function Wires(props) {
 	const [counter, setCounter] = useState(0);
@@ -14,281 +15,292 @@ export default function Wires(props) {
 
 	return (
 		<div>
-				{props.counter >= props.target ?
-					<TextInfo counter={counter} target={0} addCounter={addCounter}>
-						<table>
-							<tbody>
-								<tr >
-									<td>╔</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>╗</td>
-								</tr>
-								<tr >
-									<td >1</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>╗</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td style={{ color: "red" }}>╔</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >║</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td style={{ color: "red" }}>╚</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>═</td>
-									<td style={{ color: "red" }}>╝</td>
-									<td style={{ color: "yellow" }}>╔</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>╗</td>
-									<td style={{ color: "yellow" }}> </td>
-									<td style={{ color: "yellow" }}>╔</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >2</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>╝</td>
-									<td style={{ color: "yellow" }}> </td>
-									<td style={{ color: "yellow" }}>╚</td>
-									<td style={{ color: "yellow" }}>═</td>
-									<td style={{ color: "yellow" }}>╝</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >║</td>
-									<td style={{ color: "blue" }}>╔</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>╗</td>
-									<td> </td>
-									<td> </td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >3</td>
-									<td style={{ color: "blue" }}>╝</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td style={{ color: "blue" }}>╚</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >║</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td style={{ color: "white" }}>╔</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>╗</td>
-									<td> </td>
-									<td> </td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >4</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>╗</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td style={{ color: "white" }}>║</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td style={{ color: "white" }}>╚</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td >║</td>
-								</tr>
-								<tr>
-									<td >║</td>
-									<td> </td>
-									<td style={{ color: "white" }}>╚</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>═</td>
-									<td style={{ color: "white" }}>╝</td>
-									<td style={{ color: "blue" }}> </td>
-									<td style={{ color: "blue" }}> </td>
-									<td style={{ color: "blue" }}> </td>
-									<td style={{ color: "blue" }}> </td>
-									<td style={{ color: "blue" }}>╔</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td style={{ color: "blue" }}>═</td>
-									<td >║</td>
-								</tr>
-								<tr >
-									<td >5</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >═</td>
-									<td style={{ color: "blue" }} >╝</td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td> </td>
-									<td >║</td>
-								</tr>
-								<tr >
-									<td>╚</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>═</td>
-									<td>╝</td>
-								</tr>
-							</tbody>
-						</table>
-					</TextInfo>
-
-					:
-					<></>
-				}
+			{props.counter >= props.target ?
+				<>
+					<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column' }}>
+						<br/>
+						<TextInfo counter={counter} target={0} addCounter={addCounter}>
+							<table>
+								<tbody>
+									<tr >
+										<td>╔</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>╗</td>
+									</tr>
+									<tr >
+										<td >1</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>╗</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td style={{ color: "red" }}>╔</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >║</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td style={{ color: "red" }}>╚</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>═</td>
+										<td style={{ color: "red" }}>╝</td>
+										<td style={{ color: "yellow" }}>╔</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>╗</td>
+										<td style={{ color: "yellow" }}> </td>
+										<td style={{ color: "yellow" }}>╔</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >2</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>╝</td>
+										<td style={{ color: "yellow" }}> </td>
+										<td style={{ color: "yellow" }}>╚</td>
+										<td style={{ color: "yellow" }}>═</td>
+										<td style={{ color: "yellow" }}>╝</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >║</td>
+										<td style={{ color: "blue" }}>╔</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>╗</td>
+										<td> </td>
+										<td> </td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >3</td>
+										<td style={{ color: "blue" }}>╝</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td style={{ color: "blue" }}>╚</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >║</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td style={{ color: "white" }}>╔</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>╗</td>
+										<td> </td>
+										<td> </td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >4</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>╗</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td style={{ color: "white" }}>║</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td style={{ color: "white" }}>╚</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td >║</td>
+									</tr>
+									<tr>
+										<td >║</td>
+										<td> </td>
+										<td style={{ color: "white" }}>╚</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>═</td>
+										<td style={{ color: "white" }}>╝</td>
+										<td style={{ color: "blue" }}> </td>
+										<td style={{ color: "blue" }}> </td>
+										<td style={{ color: "blue" }}> </td>
+										<td style={{ color: "blue" }}> </td>
+										<td style={{ color: "blue" }}>╔</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td style={{ color: "blue" }}>═</td>
+										<td >║</td>
+									</tr>
+									<tr >
+										<td >5</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >═</td>
+										<td style={{ color: "blue" }} >╝</td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td> </td>
+										<td >║</td>
+									</tr>
+									<tr >
+										<td>╚</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>═</td>
+										<td>╝</td>
+									</tr>
+								</tbody>
+							</table>
+						</TextInfo>
+						<TextInfo counter={counter} target={1} addCounter={addCounter}>
+							<span style={{ color: "blue" }}>[INFO]</span> Insira apenas o número do fio que será desconectado
+						</TextInfo>
+						<br/>
+					</div>
+					<TextInput counter={counter} target={2} addCounter={props.addCounter} pass={"1"} >
+						&gt; Fio a ser desconectado:
+					</TextInput>
+				</>
+				:
+				<></>
+			}
 		</div>
 	)
 }
